a55e7f4d7af88e1d0d9b4ea49cf51792d9cbaa5f
Message <sandeshrajpoot175@gmail.com>
2025-02-10 12:08:52 +0530

    Add new file

---
+++ Hi.java
@@ -0,0 +1,6 @@
+class A {
+    public static void main(String [] args){
+        System.out.println("Sandesh Singh Rajpoot");
+        System.out.println("This is My first Github code . ");
+    }
+}
